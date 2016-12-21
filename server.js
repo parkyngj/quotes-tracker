8b79f02846001e6bfb053541c3e0de2d2dcfd24a
Young Ju Park <parkyngj@gmail.com>
2016-12-21 15:00:12 -0800

    add mongoclient connection

--- server.js
+++ server.js
@@ -2,10 +2,26 @@
 const express = require('express');
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient
+
+var db
+
+MongoClient.connect('mongodb://parkyngj:parkyngj1@ds141368.mlab.com:41368/quotes-tracker', function(err, database){
+  if(err){
+    return console.log(err)
+  }
+
+  db = database
+
+  app.listen(3000, function(){
+    console.log('listening on 3000')
+  })
+});
+
 // create server where browsers can connect to
 // do so w/ help of listen method provided by express
-app.listen(3000, function(){
-  console.log('listening on 3000')
+// app.listen(3000, function(){
+//   console.log('listening on 3000')
 })
 
 // in express, handle GET request with get method
